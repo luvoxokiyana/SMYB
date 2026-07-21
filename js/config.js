40cb246626399286b85fa0f79850a7849b93a5b3
luvoxokiyana <luvoxokiyana17@gmail.com>
2026-07-21 15:51:43 +0200

    changed to use .env keys

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 // ----------------- Config -----------------
 
 const CONFIG = {
-  SUPABASE_URL: 'https://fwmhhujiqyzwizoyjhsa.supabase.co',
-  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3bWhodWppcXl6d2l6b3lqaHNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4Njk5OTMsImV4cCI6MjA5OTQ0NTk5M30.K5tHuk4asXy-o823b3yKjYl-DZhRUfIVHJ62RtmgK0I',
-  CLOUDINARY_CLOUD_NAME: 'dkarvw3rw',
-  CLOUDINARY_UPLOAD_PRESET: 'smyb)unsigned',
-  ENV: 'production' // Change to 'production' for deployment
+  SUPABASE_URL: process.env.PUBLIC_SUPABASE_URL || 'https://fwmhhujiqyzwizoyjhsa.supabase.co',
+  SUPABASE_KEY: process.env.PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3bWhodWppcXl6d2l6b3lqaHNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4Njk5OTMsImV4cCI6MjA5OTQ0NTk5M30.K5tHuk4asXy-o823b3yKjYl-DZhRUfIVHJ62RtmgK0I',
+  CLOUDINARY_CLOUD_NAME: process.env.PUBLIC_SUPABASE_ANON_KEY || 'dkarvw3rw',
+  CLOUDINARY_UPLOAD_PRESET: process.env.PUBLIC_CLOUDINARY_UPLOAD_PRESET || 'smyb)unsigned',
+  ENV: process.env.NODE_ENV || 'development',
 };
 
 
